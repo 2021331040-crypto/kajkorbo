f2273d0b6885b19f62c321345836781d4a318a62
KAJKORBO Developer <dev@kajkorbo.com>
2025-11-27 04:14:57 +0600

    Add database table creation on server startup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import app from "./app.js";
 import { v2 as cloudinary } from "cloudinary";
+import { createTables } from "./utils/createTables.js";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
+
+// Create database tables on startup
+createTables();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
